bb9592c535dc88b7b3ffa9dd91f99fff76725b7e
Wellison Silva <wonsilva100@gmail.com>
2023-10-23 22:37:43 -0300

    token jwt de segurança criado

--- backend/src/controllers/usersControllers.js
+++ backend/src/controllers/usersControllers.js
@@ -1,6 +1,8 @@
 const { validationResult } = require('express-validator');
 const usersModel = require('../models/usersModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jwt-simple')
+const { authSecret } = require('../../.envv')
 
 const getAll = async (_req, res) => {
 
@@ -34,10 +36,6 @@ const getOneUser = async (req, res) => {
                 mensagem: "Erro: Nenhum usuário encontrado!"
             });
         });
-}
-
-const signin = async (req, res) => {
-    
 }
 
 const createUser = async (req, res) => {
@@ -101,10 +99,44 @@ const deleteUser = async (req, res) => {
 
 }
 
+// --------------- Login User --------------- //
+
+const signin = async (req, res) => {
+    if(!req.body.email || !req.body.password)  {
+        return res.status(400).send('Dados incompletos')
+    }
+
+    const user = await usersModel.Usuario.findOne({
+        where: {
+            email: req.body.email
+        },
+        
+    })
+
+    if(user){ 
+        bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+
+            if(err || !isMatch) {
+                return res.status(401).send('Senha incorreta')
+            }
+            
+            const payload = { id: user.id }
+            res.json({
+                name: user.name,
+                email: user.email,
+                token: jwt.encode(payload, authSecret)
+            })
+        })  
+    } else {
+        res.status(400).send('Usuário não encontrado')
+    }
+}
+
 module.exports = {
     getAll,
     createUser,
     deleteUser,
     updateUser,
-    getOneUser
+    getOneUser,
+    signin
 }
--- backend/src/router.js
+++ backend/src/router.js
@@ -26,23 +26,25 @@ router.get('/users/:id', userControllers.getOneUser)
 // Create User
 router.post('/users',
 //Data Validation
-    body('email').trim().notEmpty().withMessage('Campo email Vazio'),
-    body('email').isEmail().withMessage('Email Inválido'),
-    body('email').custom(async emailBody => {
-        console.log('email da req',emailBody)
-        const user = await usersModel.Usuario.findOne({ 
+body('email').trim().notEmpty().withMessage('Campo email Vazio'),
+body('email').isEmail().withMessage('Email Inválido'),
+body('email').custom(async emailBody => {
+    console.log('email da req',emailBody)
+    const user = await usersModel.Usuario.findOne({ 
             where: {
                 email: emailBody
             }
         })
-            if(user) {
-                console.log(user.dataValues.email, " = ", emailBody)
-                return Promise.reject('Email já cadastrado')
-            }
+        if(user) {
+            console.log(user.dataValues.email, " = ", emailBody)
+            return Promise.reject('Email já cadastrado')
+        }
     }),
     body('name').trim().notEmpty().withMessage('Nome Inválido'),
     userControllers.createUser)
-router.delete('/users/:id', userControllers.deleteUser)
-router.put('/users/:id', userControllers.updateUser)
-
+    router.delete('/users/:id', userControllers.deleteUser)
+    router.put('/users/:id', userControllers.updateUser)
+    // ----- Rota de Login ----- //
+    router.post('/signin' , userControllers.signin)
+    
 module.exports = router
